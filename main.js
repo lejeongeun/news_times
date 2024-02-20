a167e101963bdb7b5b0167610a08531f0ce06b1c
jeongeun lee <lje7370@gmail,com>
2024-02-20 21:39:04 +0900

    news first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const API_KEY = `941d455169354b78989df64c805f044a`
+let news = [] // 전역변수로 지정
+const getLatestNews = async () =>{
+    // url을 사용할 경우 이미 js에서 만들어둔 URL인스턴스 사용
+    const url = new URL(
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+        );
+        //fetch(): url 호출 , async await를 통해 기다려줘야 함..
+    const response = await fetch(url);
+    const data = await response.json(); // json 형식으로 받기 
+    news = data.articles;
+    console.log("ddd", news);
+};
+getLatestNews();
---
+++ main2.js
@@ -0,0 +1,6 @@
+console.log(1);
+function test(){
+    console.log(2);
+    console.log(3);
+}
+test();

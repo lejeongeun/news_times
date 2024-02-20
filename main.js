c510cd6792a0cdeb748d999374b104f528056d3f
jeongeun lee <lje7370@gmail,com>
2024-02-20 23:05:25 +0900

    first commit

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-const API_KEY = `941d455169354b78989df64c805f044a`
+// const API_KEY = `941d455169354b78989df64c805f044a`
+// https://bright-boba-89b610.netlify.app/ : my netlify 주소
 let news = [] // 전역변수로 지정
 const getLatestNews = async () =>{
-    // url을 사용할 경우 이미 js에서 만들어둔 URL인스턴스 사용
+    //URL인스턴스 사용
     const url = new URL(
-        `https://bright-boba-89b610.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`
+        `https://bright-boba-89b610.netlify.app/top-headlines?&country=kr`
         );
-        //fetch(): url 호출 , async await를 통해 기다려줘야 함..
+        
     const response = await fetch(url);
     const data = await response.json(); // json 형식으로 받기 
     news = data.articles;
@@ -13,4 +14,3 @@ const getLatestNews = async () =>{
 };
 getLatestNews();
 
-// https://bright-boba-89b610.netlify.app/

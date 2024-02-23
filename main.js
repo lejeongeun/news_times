61398b0c10d98095e7e4c9480600d8244155d805
jeongeun lee <lje7370@gmail,com>
2024-02-23 22:57:19 +0900

    add error commit

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-//const API_KEY = `941d455169354b78989df64c805f044a`
+// const API_KEY = `941d455169354b78989df64c805f044a`
 // https://bright-boba-89b610.netlify.app/ : my netlify 주소
 let newsList = [] // 전역변수로 지정
 
+// openSearchBox : search버튼을 누르면 input박스 띄우기
 const openSearchBox = () => {
     let inputArea = document.getElementById("input-area"); 
     if (inputArea.style.display === "inline") { 
@@ -11,17 +12,15 @@ const openSearchBox = () => {
     }
   };
 const searchNews = async() =>{
-    // keword변수에 search-input(검색창)에 입력한 값 받기
-    const keword = document.getElementById("search-input").value; 
-    console.log("keyword", keword);
-    const url = new URL(
-        `https://bright-boba-89b610.netlify.app/top-headlines?&country=kr&q=${keword}`
+    // keyword변수에 search-input(검색창)에 입력한 값 받기
+    const keyword = document.getElementById("search-input").value; 
+    console.log("keyword", keyword);
+    url = new URL(
+        //`https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`
+        `https://bright-boba-89b610.netlify.app/top-headlines?&country=kr&q=${keyword}`
     );
-    const response = await fetch(url);
-    const data = await response.json();
-    newsList = data.articles; // newsList에 보여주고 싶은 data인 data.articles 담기
-    render(); // ui 구현
-    console.log("keyword",newsList)
+    getNews();
+    console.log("keyword",newsList);
 };
 // 메뉴 sidebar!
 const openNav = () => {
@@ -33,35 +32,55 @@ const closeNav = () => {
 // 버튼 들고오기 
 const menus = document.querySelectorAll(".menus button");
 menus.forEach(menu => menu.addEventListener("click", (event)=>getNewsByCategory(event)));
+
+
+let url = new URL( // url을 Let 으로정의, 각 함수에서는 url을 재정의하여  getNes에서의 url로 이동
+    //`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
+    `https://bright-boba-89b610.netlify.app/top-headlines?&country=kr`
+);
+const getNews = async() =>{ // 중복 코드 리팩토링
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+        if (response.status === 200){
+            if(data.articles.length === 0){
+                throw new Error("No result for this search");
+            } 
+            newsList= data.articles;
+            render();
+        }
+        else{
+            throw new Error(data.message);
+        }
+    }
+    catch(error){
+        errorRender(error.message);
+
+    };
+};
 const getLatestNews = async () =>{
     //URL인스턴스 사용
-    const url = new URL(
-        //`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+    url = new URL(
+        //`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
         `https://bright-boba-89b610.netlify.app/top-headlines?&country=kr`
         );
-        
-    const response = await fetch(url);
-    const data = await response.json(); // json 형식으로 받기 
-    newsList = data.articles;
-    render(); // newsList가 확정되는 부분에 적용
-    console.log("ddd", newsList);
-    
-
+    getNews();
 };
 // 카테고리에 click 이벤트 부여 
 const getNewsByCategory = async (event) => {
     const category = event.target.textContent.toLowerCase(); // event로 category받아오기
     console.log("category", category);
-    const url = new URL(
+    url = new URL(
+        //`https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`
         `https://bright-boba-89b610.netlify.app/top-headlines?&country=kr&category=${category}`
         );
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log("ddd", data);
-        newsList= data.articles;
-        render();
-
+    getNews();
 };
+/**
+에러 핸들링
+1. 받은 api데이터가 0개라면 화면에 no matches for your search
+2. 받은 응답 코드가 200이 아니라면 (400, 401, 402등) 받은 에러 메세지를 화면에 보여주기
+ */
 const render = () => {
     const newsHTML = newsList.map((news) => {
         let truncatedDescription = news.description ? 
@@ -84,6 +103,13 @@ const render = () => {
 
     document.getElementById("news-board").innerHTML = newsHTML;
 };
+const errorRender = (errorMessage) =>{
+    // 부트스트랩 사용
+    const errorHTML = `<div class="alert alert-danger" role="alert">
+    ${errorMessage}
+  </div>`;
+  document.getElementById("news-board").innerHTML=errorHTML
+}
 
 getLatestNews();
 

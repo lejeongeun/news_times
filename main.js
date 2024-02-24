5e8036c05f673b0f8fdb1e62cdf63f6b7b6fd66b
jeongeun lee <lje7370@gmail,com>
2024-02-24 21:30:23 +0900

    add pagination

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-// const API_KEY = `941d455169354b78989df64c805f044a`
+const API_KEY = `941d455169354b78989df64c805f044a`
 // https://bright-boba-89b610.netlify.app/ : my netlify 주소
 let newsList = [] // 전역변수로 지정
+let totalResults = 0;
+// 임의로 정해주는 값
+let page = 1; // 변동 가능
+const pageSize = 10; // const인 이유: 10개의 page bar는 바뀔수 없음, 고정된 값 
+const groupSize = 5; // 바뀔 수 없음, 고정된 값
+
 
 // openSearchBox : search버튼을 누르면 input박스 띄우기
 const openSearchBox = () => {
@@ -16,8 +22,8 @@ const searchNews = async() =>{
     const keyword = document.getElementById("search-input").value; 
     console.log("keyword", keyword);
     url = new URL(
-        //`https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`
-        `https://bright-boba-89b610.netlify.app/top-headlines?&country=kr&q=${keyword}`
+        `https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`
+        //`https://bright-boba-89b610.netlify.app/top-headlines?&country=kr&q=${keyword}`
     );
     getNews();
     console.log("keyword",newsList);
@@ -35,11 +41,16 @@ menus.forEach(menu => menu.addEventListener("click", (event)=>getNewsByCategory(
 
 
 let url = new URL( // url을 Let 으로정의, 각 함수에서는 url을 재정의하여  getNes에서의 url로 이동
-    //`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
-    `https://bright-boba-89b610.netlify.app/top-headlines?&country=kr`
+    `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
+    //`https://bright-boba-89b610.netlify.app/top-headlines?&country=kr`
 );
 const getNews = async() =>{ // 중복 코드 리팩토링
     try{
+        //url을 받음과 동시에 page정보 붙여주기 
+        // searchParams.set(): 페이지라는 파라미터 세팅
+        url.searchParams.set("page", page);
+        url.searchParams.set("pageSize", pageSize);
+
         const response = await fetch(url);
         const data = await response.json();
         if (response.status === 200){
@@ -47,7 +58,10 @@ const getNews = async() =>{ // 중복 코드 리팩토링
                 throw new Error("No result for this search");
             } 
             newsList= data.articles;
+            // 데이터가 있고 length가 0보다 크다면? 
+            totalResults = data.totalResults;
             render();
+            paginationRender();// news가 나온 후 pagination 구성
         }
         else{
             throw new Error(data.message);
@@ -55,14 +69,13 @@ const getNews = async() =>{ // 중복 코드 리팩토링
     }
     catch(error){
         errorRender(error.message);
-
     };
 };
 const getLatestNews = async () =>{
     //URL인스턴스 사용
     url = new URL(
-        //`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
-        `https://bright-boba-89b610.netlify.app/top-headlines?&country=kr`
+        `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
+        //`https://bright-boba-89b610.netlify.app/top-headlines?&country=kr`
         );
     getNews();
 };
@@ -71,16 +84,12 @@ const getNewsByCategory = async (event) => {
     const category = event.target.textContent.toLowerCase(); // event로 category받아오기
     console.log("category", category);
     url = new URL(
-        //`https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`
-        `https://bright-boba-89b610.netlify.app/top-headlines?&country=kr&category=${category}`
+        `https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`
+        //`https://bright-boba-89b610.netlify.app/top-headlines?&country=kr&category=${category}`
         );
     getNews();
 };
-/**
-에러 핸들링
-1. 받은 api데이터가 0개라면 화면에 no matches for your search
-2. 받은 응답 코드가 200이 아니라면 (400, 401, 402등) 받은 에러 메세지를 화면에 보여주기
- */
+
 const render = () => {
     const newsHTML = newsList.map((news) => {
         let truncatedDescription = news.description ? 
@@ -110,6 +119,60 @@ const errorRender = (errorMessage) =>{
   </div>`;
   document.getElementById("news-board").innerHTML=errorHTML
 }
+const paginationRender = () =>{
+    // totalResult: data의 totalResult값이 있음
+    //page
+    //pageSize
+    // goupSize --> 위에 변수로 초기화 
+    // pagGroup
+    const pageGroup = Math.ceil(page /groupSize);
+    // totalPages
+    const totalPages = Math.ceil(totalResults/pageSize);
+    // lastPage
+    let lastPage = pageGroup * groupSize;
+    // 마지막 페이지 그룹이 그룹 사이즈보다 작을경우 lastPage = totalPages 설정
+    if(lastPage > totalPages){
+        lastPage = totalPages;
+    }
+    //firstpage
+    const firstPage = lastPage - (groupSize -1) <= 0? 1: lastPage - (groupSize -1); 
+    // firstPage<groupSize일 경우() 1로 설정 lastPage - (groupSize -1) <= 0? 1 :
+    // pagination
+    // getNews 함수 안에 new가 나오는 render() 아래 paginationHTML호출
+
+    let paginationHTML = ``
+    if (page > 1) {
+        paginationHTML += `<li class="page-item" onclick="moveToPage(${page - 1}"><a class="page-link">&lt;</a></li>`;
+    }
+    for(let i = firstPage; i<=lastPage; i++){
+        paginationHTML += ` <li class="page-item ${
+            i === page ? "active" : ""
+        }" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`
+    }
+    if (page < totalPages) {
+        paginationHTML += `<li class="page-item" onclick="moveToPage(${page + 1})"><a class="page-link">&gt;</a></li>`;
+    }
+    document.querySelector(".pagination").innerHTML = paginationHTML;
+};
+    // pagination
+/* <nav aria-label="Page navigation example">
+  <ul class="pagination">
+    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+  </ul>
+</nav> */
+
+const moveToPage=(pageNum)=>{ 
+    // 클릭한 page번호를 알기 위해 onclick에 ${i} 지정 -> 이를 매개변수 pageNum으로 받기 
+    // url을 호출해줘야 하므로 getNews 호출, 
+    // pageNum이 변하므로 getNews에 페이지 번호 지정
+    console.log("movetoPage", pageNum);
+    page = pageNum; // 페이지가 1로 지정되어있는 값을 pageNum에 따라 바뀌도록 지정
+    getNews();
+}
 
 getLatestNews();
 
@@ -117,4 +180,5 @@ getLatestNews();
 1. 버튼에 클릭 이벤트주기
 2. 카테고리별 News 가져오기
 3. 그 뉴스 보여주기
+
  */
